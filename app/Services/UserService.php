4c8dd353661700496e5bb387d6705ee50feeb367
Yrbre <bariqfajar60@gmail.com>
2026-04-23 13:20:01 +0700

    ADD: User Service

---
+++ app/Services/UserService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Services\Interfaces\UserServiceInterface;
+
+class UserService implements UserServiceInterface
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->userRepository->getAll();
+    }
+
+    public function getById($id)
+    {
+        return $this->userRepository->getById($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->userRepository->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        return $this->userRepository->update($id, $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->userRepository->delete($id);
+    }
+}
