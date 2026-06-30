fecec4305f2c9baa316f3617289263c5768e5ca2
Yrbre <bariqfajar60@gmail.com>
2026-06-30 10:42:04 +0700

    Modif: Migration Transfer_Request

--- database/migrations/2026_06_30_095138_create_transfer_requests_table.php
+++ database/migrations/2026_06_30_095138_create_transfer_requests_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('transfer_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('item_id')->constrained('ic_item_mst')->onDelete('cascade');
+            $table->foreignId('item_id')->nullable()->constrained('ic_item_mst')->onDelete('cascade');
             $table->double('requested_qty')->nullable();
-            $table->foreignId('source_warehouse_id')->constrained('warehouses')->onDelete('cascade');
-            $table->foreignId('destination_warehouse_id')->constrained('warehouses')->onDelete('cascade');
-            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
-            $table->foreignId('requested_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('source_warehouse_id')->nullable()->constrained('warehouses')->onDelete('cascade');
+            $table->foreignId('destination_warehouse_id')->nullable()->constrained('warehouses')->onDelete('cascade');
+            $table->foreignId('department_id')->nullable()->constrained('departments')->onDelete('cascade');
+            $table->foreignId('requested_by')->nullable()->constrained('users')->onDelete('cascade');
             $table->string('status')->default('pending');
             $table->date('requested_date')->nullable();
             $table->text('notes')->nullable();
